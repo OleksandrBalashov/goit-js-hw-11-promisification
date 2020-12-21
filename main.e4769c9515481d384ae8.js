c21035bbb467db80b70fc24e2568691026af94a8
OleksandrBalashov <aleksandr.balashov.sax@gmal.com>
2020-12-21 23:36:32 +0200

    Updates

--- main.e4769c9515481d384ae8.js
+++
@@ -1,2 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[0],{L1EO:function(n,e,a){},QfWi:function(n,e,a){"use strict";a.r(e);a("L1EO"),a("lmye"),a("D/wG"),a("wCa+"),a("JBxO"),a("FdtR");var t=[{name:"Mango",active:!0},{name:"Poly",active:!1},{name:"Ajax",active:!0},{name:"Lux",active:!1}],i=function(n,e){var a=n.map((function(n){return n.name===e?Object.assign({},n,{active:!n.active}):n}));return new Promise((function(n){n(a)}))},c=function(n){return console.table(n)};i(t,"Mango").then(c),i(t,"Lux").then(c)}},[["QfWi",1,2]]]);
-//# sourceMappingURL=main.e4769c9515481d384ae8.js.map
